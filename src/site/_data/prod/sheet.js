025e1d5085314aa823dc6595ea3f56a23b8c5233
Spencer Haizel <spencer@haizel.co.uk>
2020-10-23 14:38:53 +0100

    initial init

---
+++ src/site/_data/prod/sheet.js
@@ -0,0 +1,51 @@
+const axios  = require('axios');
+const seed   = require('../../../utils/save-seed.js');
+
+
+// Once a googel sheet is "published to the web" we can access its JSON
+// via a URL of this form. We just need to pass in the ID of the sheet
+// which we can find in the URL of the document.
+const sheetID = "1HpW7adgxIBxiXaf24TE5bHgmClPmIbEGMkbvjKuD_uA";
+const googleSheetUrl = `https://spreadsheets.google.com/feeds/list/${sheetID}/ob3uqk0/public/values?alt=json`;
+
+module.exports = () => {
+  return new Promise((resolve, reject) => {
+
+    console.log(`Requesting data from ${googleSheetUrl}`);
+
+    axios.get(googleSheetUrl)
+      .then(response => {
+
+        // massage the data from the Google Sheets API into
+        // a shape that will more convenient for us in our SSG.
+        var data = {
+          "content": []
+        };
+
+        response.data.feed.entry.forEach(item => {
+          data.content.push({
+            "name": item.gsx$name.$t,
+            "location": item.gsx$location.$t,
+            "phonenumber": item.gsx$phonenumber.$t,
+            "twitterhandle": item.gsx$twitterhandle.$t,
+            "instagramhandle": item.gsx$instagramhandle.$t,
+            "facebookhandle": item.gsx$facebookhandle.$t
+          })
+        });
+
+        // stash the data locally for developing without
+        // needing to hit the API each time.
+        seed(JSON.stringify(data), `${__dirname}/../dev/sheet.json`);
+
+        // resolve the promise and return the data
+        resolve(data);
+
+      })
+
+      // uh-oh. Handle any errrors we might encounter
+      .catch(error => {
+        console.log('Error :', error);
+        reject(error);
+      });
+  })
+}
---
+++ src/site/index.md
@@ -0,0 +1,19 @@
+---
+title: Restaurants offering free meals to children
+layout: default
+---
+
+<div class="restaurants">
+  <div class="restaurants__column">
+    <ul class="listing">
+      {%- for item in sheet.content -%}
+        <li>
+          <strong>{{ item.name }}, {{ item.location }}</strong>
+          <span>{% if item.twitterhandle %}<a href="https://twitter.com/{{ item.twitterhandle }}" target="_blank">Twitter</a>, {% endif %}{% if item.instagramhandle %}<a href="https://instagram.com/{{ item.instagramhandle }}" target="_blank">Instagram</a>, {% endif %}{% if item.facebookhandle %}<a href="https://facebook.com/{{ item.facebookhandle }}" target="_blank">Facebook</a>, {% endif %} <a href="tel:{{ item.phonenumber }}">Telephone</a></span>
+        </li>
+      {%- endfor -%}
+    </ul>
+  </div>
+</div>
+
+A quick [Eleventy](https://www.11ty.io/) project built by [Spencer Haizel](https://twitter.com/spenncerr) inspired by [Andy Bell](https://twitter.com/hankchizljaw) with data collected by various methods.
